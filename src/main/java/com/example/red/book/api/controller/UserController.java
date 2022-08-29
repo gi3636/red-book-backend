7bad209ebe9c7c874509689dc457bd24b932527a
franky <franky.b@iscmango.com>
2022-08-29 17:29:11 +0800

    feat: 1.删除没必要的东西 2.调通代码生产器 3.添加了tbl_user的sql 4.生成了tbl_user表的代码 5.更改数据库与密码

---
+++ src/main/java/com/example/red/book/api/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.red.book.api.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author franky
+ * @since 2022-08-29
+ */
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+}
+
