a605e56b1d3860321466a52568b1280cc1681db7
gi3636 <fenggi123@gmail.com>
2022-09-11 13:57:35 +0800

    feat: 1.用户更新接口 添加验证

--- src/main/java/com/example/red/book/controller/UserController.java
+++ src/main/java/com/example/red/book/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.red.book.model.vo.UserVO;
 import com.example.red.book.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
  * <p>
@@ -25,6 +27,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Api(tags = "用户模块")
 @RestController
+@Slf4j
 @RequestMapping("/api/user")
 public class UserController {
 
@@ -37,6 +40,10 @@ public class UserController {
     @ApiOperation(value = "用户资料更新")
     @PostMapping("update")
     public CommonResult<Boolean> updateUser(@Validated @RequestBody UserVO userVO) {
+        Long id = (Long) request.getSession().getAttribute("id");
+        if (!Objects.equals(userVO.getId(), id)){
+            return CommonResult.failed("无权限");
+        }
         boolean isSuccess = userService.updateUserById(userVO);
         if (!isSuccess) {
             return CommonResult.failed("修改失败");
