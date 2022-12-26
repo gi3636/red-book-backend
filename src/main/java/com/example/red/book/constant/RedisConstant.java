a9bf5ea93955ea0dd71103656e7ee9696045ac8f
franky <franky.b@iscmango.com>
2022-12-26 10:02:08 +0800

    feat:测试webhook

--- src/main/java/com/example/red/book/constant/RedisConstant.java
+++ src/main/java/com/example/red/book/constant/RedisConstant.java
@@ -5,5 +5,4 @@ public class RedisConstant {
     public static final String REDIS_DATABASE = "red_book";
     public static final Integer REDIS_EXPIRE = 86400;
     public static final String REDIS_KEY_USER = "user";
-
 }
